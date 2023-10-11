8f768c78b1bd2740f078603b87303d031eb6683b
chastycaacoy <caacoy.chastymaeg.10022002@gmail.com>
2023-10-11 09:44:25 +0800

    final modification

--- BSIT3L-Movies/Models/MoviesModel.cs
+++ BSIT3L-Movies/Models/MoviesModel.cs
@@ -9,7 +9,10 @@ namespace BSIT3L_Movies.Models
         public string Rating { get; set; }
         public int ReleaseYear { get; set; }
         public string Genre { get; set; }
-        
+        public string Description { get; set; }
+        public string PosterImageUrl { get; set; }
+        public string TrailerUrl { get; set; }
+
     }
 }
 
